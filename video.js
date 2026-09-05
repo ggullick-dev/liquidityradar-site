a60ccaaba5f57ef2fc8e42b414e514ef06404bf1
elgulcho <ggullick@googlemail.com>
2026-09-05 15:37:59 +0400

    Expand site around correlated liquidity risk

--- video.js
+++ video.js
@@ -1,9 +1,11 @@
 const dialog = document.querySelector('.video-dialog');
-const openButton = document.querySelector('[data-video-open]');
+const openButtons = document.querySelectorAll('[data-video-open]');
 const closeButton = document.querySelector('[data-video-close]');
 const player = dialog.querySelector('video');
+let activeTrigger = openButtons[0];
 
-function openVideo() {
+function openVideo(event) {
+  activeTrigger = event.currentTarget;
   dialog.showModal();
   player.currentTime = 0;
   player.play().catch(() => {});
@@ -15,7 +17,7 @@ function closeVideo() {
   dialog.close();
 }
 
-openButton.addEventListener('click', openVideo);
+openButtons.forEach((button) => button.addEventListener('click', openVideo));
 closeButton.addEventListener('click', closeVideo);
 
 dialog.addEventListener('click', (event) => {
@@ -25,5 +27,5 @@ dialog.addEventListener('click', (event) => {
 dialog.addEventListener('close', () => {
   player.pause();
   player.currentTime = 0;
-  openButton.focus();
+  activeTrigger.focus();
 });
